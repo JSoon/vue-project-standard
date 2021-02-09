b3f9380737a36d4f09f9142312ef4707b019dbca
sunjing <serdeemail@gmail.com>
2021-02-09 14:56:54 +0800

    更新babel配置文件

--- babel.config.js
+++ babel.config.js
@@ -1,16 +1,24 @@
 module.exports = {
   presets: [
-    // '@vue/cli-plugin-babel/preset',
+    // 方式1: 包含 Babel 7 + babel-loader + @vue/babel-preset-app
+    // https://www.npmjs.com/package/@vue/cli-plugin-babel
+    // [
+    //   '@vue/cli-plugin-babel/preset', {
+    //     useBuiltIns: 'entry',
+    //   },
+    // ],
+    // 方式2: 精准配置
+    // This preset is meant to be used exclusively in projects created via Vue CLI and does not consider external use cases.
+    // https://www.npmjs.com/package/@vue/babel-preset-app
     // Babel Polyfills
     // https://cli.vuejs.org/guide/browser-compatibility.html
     [
-      '@vue/app', {
+      '@vue/babel-preset-app', {
         useBuiltIns: 'entry',
-        // corejs: {
-        //   version: 3,
-        //   proposals: true,
-        // },
       },
     ],
+  ],
+  plugins: [
+
   ],
 };
